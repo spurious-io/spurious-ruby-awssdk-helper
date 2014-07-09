dda9d1b62d58f69c6d58b1a6d5d2ddcbb7beca40
Steven Jack <stevenmajack@gmail.com>
2014-07-09 16:45:14 +0100

    Initial commit

---
+++ lib/spurious/ruby/awssdk/helper.rb
@@ -0,0 +1,11 @@
+require "spurious/ruby/awssdk/helper/version"
+
+module Spurious
+  module Ruby
+    module Awssdk
+      module Helper
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/spurious/ruby/awssdk/helper/version.rb
@@ -0,0 +1,9 @@
+module Spurious
+  module Ruby
+    module Awssdk
+      module Helper
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end

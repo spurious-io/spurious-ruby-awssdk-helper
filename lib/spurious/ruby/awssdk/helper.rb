157a18d80a3d368e0d7b8ed482ce54c587deaa7f
Steven Jack <stevenmajack@gmail.com>
2014-07-09 16:46:06 +0100

    Parse spurious output
    Closes #1

--- lib/spurious/ruby/awssdk/helper.rb
+++ lib/spurious/ruby/awssdk/helper.rb
@@ -1,10 +1,25 @@
 require "spurious/ruby/awssdk/helper/version"
+require 'spurious/ruby/awssdk/strategy'
+require "aws-sdk"
+require "json"
 
 module Spurious
   module Ruby
     module Awssdk
       module Helper
-        # Your code goes here...
+
+        def self.port_config()
+          config = `spurious ports --json`
+          JSON.parse(config)
+        rescue Exception
+          raise("The spurious CLI tool didn't return the port configuration")
+        end
+
+        def self.configure(strategy = nil)
+          strategy ||= Spurious::Ruby::Awssdk::Strategy.new(true)
+          strategy.apply(port_config)
+        end
+
       end
     end
   end

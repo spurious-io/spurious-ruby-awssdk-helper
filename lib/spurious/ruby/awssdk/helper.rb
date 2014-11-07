93bbbf722dac440bbcd5112f1fac57d451aa9595
Steven Jack <stevenmajack@gmail.com>
2014-11-07 09:29:12 +0000

    Adds docker config type from docker env variables

--- lib/spurious/ruby/awssdk/helper.rb
+++ lib/spurious/ruby/awssdk/helper.rb
@@ -8,16 +8,53 @@ module Spurious
     module Awssdk
       module Helper
 
-        def self.port_config()
+        def self.port_config
           config = `spurious ports --json`
           JSON.parse(config)
         rescue Exception
           raise("The spurious CLI tool didn't return the port configuration")
         end
 
-        def self.configure(strategy = nil)
+        def self.docker_config
+          regex     = /\/(?<host>[0-9\.]+):(?<port>[0-9]+)/
+          dynamo_db = regex.match(ENV['SPURIOUS_DYNAMODB_PORT'])
+          s3        = regex.match(ENV['SPURIOUS_S3_PORT'])
+          sqs       = regex.match(ENV['SPURIOUS_SQS_PORT'])
+
+          {
+            "spurious-dynamo" => [
+              {
+                "Host"     => dynamo_db[:host],
+                "HostPort" => dynamo_db[:port]
+              }
+            ],
+            "spurious-sqs" => [
+              {
+                "Host"     => sqs[:host],
+                "HostPort" => sqs[:port]
+              }
+            ],
+            "spurious-s3" => [
+              {
+                "Host"     => s3[:host],
+                "HostPort" => s3[:port]
+              }
+            ]
+          }
+        end
+
+        def self.config(type)
+          case type
+          when :cli
+            port_config
+          when :docker
+            docker_config
+          end
+        end
+
+        def self.configure(type = :cli, strategy = nil)
           strategy ||= Spurious::Ruby::Awssdk::Strategy.new(true)
-          strategy.apply(port_config)
+          strategy.apply(config(type))
         end
 
       end

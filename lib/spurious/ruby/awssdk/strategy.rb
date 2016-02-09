229178d3e92df832b533d5037fc6e52ebdf4dfb4
Charlie Revett <charlierevett@gmail.com>
2016-02-09 13:02:56 +0000

    Consistent quotes

--- lib/spurious/ruby/awssdk/strategy.rb
+++ lib/spurious/ruby/awssdk/strategy.rb
@@ -17,34 +17,34 @@ module Spurious
         end
 
         def dynamo(port = true, ip = true)
-          mapping['spurious-dynamo'] = {
-            'port'       => port,
-            'ip'         => ip,
-            'identifier' => 'dynamo_db'
+          mapping["spurious-dynamo"] = {
+            "port"       => port,
+            "ip"         => ip,
+            "identifier" => "dynamo_db"
           }
         end
 
         def sqs(port = true, ip = true)
-          mapping['spurious-sqs'] = {
-            'port'       => port,
-            'ip'         => ip,
-            'identifier' => 'sqs'
+          mapping["spurious-sqs"] = {
+            "port"       => port,
+            "ip"         => ip,
+            "identifier" => "sqs"
           }
         end
 
         def s3(port = true, ip = true)
-          mapping['spurious-s3'] = {
-            'port'       => port,
-            'ip'         => ip,
-            'identifier' => 's3'
+          mapping["spurious-s3"] = {
+            "port"       => port,
+            "ip"         => ip,
+            "identifier" => "s3"
           }
         end
 
         def apply(config)
           mapping.each do |type, mappings|
             ports = config[type]
-            Aws.config("#{mappings['identifier']}_port".to_sym => ports.first['HostPort']) if mappings['port']
-            Aws.config("#{mappings['identifier']}_endpoint".to_sym => ports.first['Host']) if mappings['ip']
+            Aws.config("#{mappings['identifier']}_port".to_sym => ports.first["HostPort"]) if mappings["port"]
+            Aws.config("#{mappings['identifier']}_endpoint".to_sym => ports.first["Host"]) if mappings["ip"]
           end
 
           Aws.config(:use_ssl => false, :s3_force_path_style => true)

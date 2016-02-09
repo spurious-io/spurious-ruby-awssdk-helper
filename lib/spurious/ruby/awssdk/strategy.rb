e617bcf1f8fe480948fe8795c715af7440716783
Charlie Revett <charlierevett@gmail.com>
2016-02-09 13:04:45 +0000

    Organise methods

--- lib/spurious/ruby/awssdk/strategy.rb
+++ lib/spurious/ruby/awssdk/strategy.rb
@@ -5,8 +5,6 @@ module Spurious
   module Ruby
     module Awssdk
       class Strategy
-        attr_accessor :mapping
-
         def initialize(set_all = false)
           @mapping = {}
           return unless set_all
@@ -16,19 +14,25 @@ module Spurious
           s3
         end
 
+        def apply(config)
+          mapping.each do |type, mappings|
+            ports = config[type]
+            Aws.config("#{mappings['identifier']}_port".to_sym => ports.first["HostPort"]) if mappings["port"]
+            Aws.config("#{mappings['identifier']}_endpoint".to_sym => ports.first["Host"]) if mappings["ip"]
+          end
+
+          Aws.config(:use_ssl => false, :s3_force_path_style => true)
+        end
+
+        private
+
+        attr_reader :mapping
+
         def dynamo(port = true, ip = true)
           mapping["spurious-dynamo"] = {
             "port"       => port,
             "ip"         => ip,
             "identifier" => "dynamo_db"
-          }
-        end
-
-        def sqs(port = true, ip = true)
-          mapping["spurious-sqs"] = {
-            "port"       => port,
-            "ip"         => ip,
-            "identifier" => "sqs"
           }
         end
 
@@ -40,14 +44,12 @@ module Spurious
           }
         end
 
-        def apply(config)
-          mapping.each do |type, mappings|
-            ports = config[type]
-            Aws.config("#{mappings['identifier']}_port".to_sym => ports.first["HostPort"]) if mappings["port"]
-            Aws.config("#{mappings['identifier']}_endpoint".to_sym => ports.first["Host"]) if mappings["ip"]
-          end
-
-          Aws.config(:use_ssl => false, :s3_force_path_style => true)
+        def sqs(port = true, ip = true)
+          mapping["spurious-sqs"] = {
+            "port"       => port,
+            "ip"         => ip,
+            "identifier" => "sqs"
+          }
         end
       end
     end
